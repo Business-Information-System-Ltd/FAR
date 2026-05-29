923e525bdb7432b8806af53a542c090e98190cdb
AnnaThunn <ngwesinthunn1@gmail.com>
2026-05-29 09:03:31 +0630

    backend validate

--- FAR/config/urls.py
+++ FAR/config/urls.py
@@ -17,24 +17,21 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from apps.master_data.department.meta.department_field_meta_view import DepartmentFieldMetaView  
 
 from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/far/v1/', include([
+    
+    
+    path('api/far/v1/meta/', include([
         path('branches/', include ('apps.master_data.branch.api.urls')),
         path('countries/', include ('apps.master_data.country.api.urls')),
         path('currencies/', include ('apps.master_data.currency.api.urls')),
-<<<<<<< HEAD
-        path('branches/', include ('apps.master_data.branch.api.urls')),
-        path('departments/', include ('apps.master_data.department.api.urls')),
-        path('locations/', include ('apps.master_data.location.api.urls')),
-        path('countries/', include ('apps.master_data.country.api.urls')),
-        
-=======
+
         path('custodians/', include ('apps.master_data.custodian.api.urls')),
         path('departments/', include ('apps.master_data.department.api.urls')),
         path('locations/', include ('apps.master_data.location.api.urls')),
->>>>>>> bc5b9b977497e63123828f82cb77dcc39ea37a8e
     ])),
+    
 ]
